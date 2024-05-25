1bf1b392db5a5a29957baf35543b31e159b32ba7
hieumai1507 <114709586+hieumai1507@users.noreply.github.com>
2024-05-25 17:58:16 +0700

    fix wishlist

--- app/src/main/java/com/example/appbanlaptop/fragment/SearchFragment.java
+++ app/src/main/java/com/example/appbanlaptop/fragment/SearchFragment.java
@@ -32,6 +32,7 @@ import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.appbanlaptop.R;
 import com.example.appbanlaptop.manager.CartManager;
+import com.example.appbanlaptop.manager.WishListManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -41,6 +42,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class SearchFragment extends Fragment {
 
@@ -217,6 +219,17 @@ public class SearchFragment extends Fragment {
         public double getDiscount() {
             return discount;
         }
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            LaptopProduct that = (LaptopProduct) o;
+            return id == that.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
     }
 
     public static class LaptopProductAdapter extends ArrayAdapter<LaptopProduct> {
@@ -262,11 +275,16 @@ public class SearchFragment extends Fragment {
                 });
 
                 wishListButton.setOnClickListener(new View.OnClickListener() {
-                    //fix wishlit k bi trung lap
                     @Override
                     public void onClick(View v) {
-                        CartManager.getInstance().addToWishList(product);
-                        Toast.makeText(getContext(), product.getName() + " đã được thêm vào danh sách yêu thích", Toast.LENGTH_SHORT).show();
+                        WishListManager wishListManager = WishListManager.getInstance();
+                        List<LaptopProduct> wishListItems = wishListManager.getWishListItems();
+                        if (!wishListItems.contains(product)) {
+                            wishListManager.addToWishList(product);
+                            Toast.makeText(getContext(), product.getName() + " đã được thêm vào danh sách yêu thích", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), product.getName() + " đã có trong danh sách yêu thích", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
             }
